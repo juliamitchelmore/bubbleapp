e658aa3dfd0ce4d705b8950d0193ca4fbea8be63
Stefan <stefan.caliaro@gmail.com>
2015-09-05 14:01:51 +1000

    Added FB Sign-in auth step for the app.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function()
 {
 	var fb = new Firebase("https://scorching-heat-529.firebaseio.com/");
+		fb.authWithOAuthPopup("facebook", function(error, authData) {
+		  if (error) {
+		    console.log("Login Failed!", error);
+		  } else {
+		    console.log("Authenticated successfully with payload:", authData);
+		  }
+		});
+
 
 	$('#messageInput').keypress(function (e) {
 	    if (e.keyCode == 13) {
