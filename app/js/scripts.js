b66a92a5e7eaa1fdaa19b04fc8bf491e9b267cf2
Julia Mitchelmore <julia@juliamitchelmore.com>
2015-09-05 15:54:37 +1000

    attempt to add a new user - needs testing

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -19,14 +19,35 @@ $(document).ready(function()
 	  });
 
 	fb.on('child_added', function(snapshot) {
-	  var message = snapshot.val();
+		var message = snapshot.val();
         displayChatMessage(message.text);
-      });
+    });
   
   	function displayChatMessage(text) {
   		var addMessage="<div class='message left'>" + text + "</div>";
     	$(addMessage).appendTo($('#messagesDiv'));
     	$(document).scrollTop($(document).height());
   	};
+
+
+
+  	//add a user
+  	var isNewUser = true;
+  	var userFB = '';
+  	var color = randomColor();
+  	console.log(color);
+	fb.onAuth(function(authData) {
+	  if (authData && isNewUser) {
+	    // save the user's profile into the database so we can list users,
+	    // use them in Security and Firebase Rules, and show profiles
+	    fb.child(authData.uid).set({
+	      provider: authData.provider,
+	      name: getName(authData),
+	      color: color
+	    });
+
+	    userFB = new Firebase("https://scorching-heat-529.firebaseio.com/" + authData.uid);
+	  }
+	});
 
 });
