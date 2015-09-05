9bbaad62e9f8404bf1fcbbe9aa7545c71ac0a186
Julia Mitchelmore <julia@juliamitchelmore.com>
2015-09-05 13:43:24 +1000

    Basic chat function for Firebase

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,6 +2,21 @@ $(document).ready(function()
 {
 	var fb = new Firebase("https://scorching-heat-529.firebaseio.com/");
 
-	
+	$('#messageInput').keypress(function (e) {
+	    if (e.keyCode == 13) {
+	      var text = $('#messageInput').val();
+	      fb.push({text: text});
+	      $('#messageInput').val('');
+	    }
+	  });
+
+	fb.on('child_added', function(snapshot) {
+	  var message = snapshot.val();
+        displayChatMessage(message.text);
+      });
+  
+  	function displayChatMessage(text) {
+    	$('<div/>').text(text).appendTo($('#messagesDiv'));
+  	};
 
 });
