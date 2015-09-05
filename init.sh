1e8bba93d22e8ab546df091f825c77f6703802e0
Tim Stokes <tim@mogeneration.com>
2015-09-05 14:55:58 +1000

    fix init.sh to install was lci

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@ echo Checking AWS CLI
 if ! type "aws" > /dev/null; then
 echo Installing AWS CLI
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip > /tmp/awscli-bundle.zip
-unzip /tmp/awscli-bundle.zip 
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+unzip /tmp/awscli-bundle.zip -d /tmp
+cd /tmp/awscli-bundle
+sudo .install
 rm -R awscli-bundle  
 fi
 echo OK
